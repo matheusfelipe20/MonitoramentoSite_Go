a7c25a10f68939d87ead06e34cd43875529ca624
Matheus Felipe <felipe_2012matheus@hotmail.com>
2022-05-04 00:01:22 -0300

    master

--- monitoramentoSites.go
+++ monitoramentoSites.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -11,8 +12,8 @@ import (
 	"time"
 )
 
-const monitoramento = 3
-const delayMonitoramento = 2
+const monitoramento = 3       //Quantas vezes será monitorado quando for solicitado
+const delayMonitoramento = 10 //A cada x segundos será monitorado novamente
 
 func main() {
 
@@ -28,6 +29,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do Programa")
 			os.Exit(0)
@@ -134,4 +136,15 @@ func regristraLog(site string, status bool) {
 	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
 }
